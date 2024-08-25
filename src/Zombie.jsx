d4d0877b4b3ecdd7f6ae367e83b6164e8ab27b52
Ito Aghayere <itohan.aghayere@gmail.com>
2024-08-25 12:42:38 -0500

    builds total strength and total agility display

--- src/Zombie.jsx
+++ src/Zombie.jsx
@@ -4,16 +4,19 @@ import { zombieFighters } from "./data.js";
 export default function Zombie() {
     const [money, setMoney] = useState(100);
     const [team, setTeam] = useState([]);
+    const [totalStrength, setTotalStrength] = useState(0)
+    const [totalAgility, setTotalAgility] = useState(0)
 
 function handleAddClick(fighter) {
     if (money >= fighter.price) {
         setTeam([...team, fighter])
         setMoney(money - fighter.price)
+        setTotalStrength(totalStrength + fighter.strength)
+        setTotalAgility(totalAgility + fighter.agility)
     } else {
         console.log("Not enough money") // not sure if I need this, but just in case I'll leave it.
-        }
     }
-
+  }
   /*
   function handleMoreClick() {
     setShowMore(!showMore);
@@ -32,6 +35,8 @@ return (
     <div>
     <h1>Zombie Fighters</h1>
     {money < 10 ? (<h2>Not enough money</h2>) : (<h2>Money: ${money}</h2>)}
+    {totalStrength === 0 ? (<h2>No strength</h2>) : (<h2>Strength: {totalStrength}</h2>)}
+    {totalAgility === 0 ? (<h2>No agility</h2>) : (<h2>Agility: {totalAgility}</h2>)}
     <ul>
         {zombieFighters.map((fighter, index) => (
             <li key={`${fighter.name}-${index}`}>
